c31491f883c965b182c42112957afe31666baf7c
Stan-breaks <stanleymwendwa03@gmail.com>
2025-02-16 12:15:22 +0300

    main.go
    handle the root route

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module nimblestack
 
 go 1.24.0
+
+require github.com/a-h/templ v0.3.833 // indirect
--- main.go
+++ main.go
@@ -1,1 +1,28 @@
 package main
+
+import (
+	"log"
+	"net/http"
+	"nimblestack/views"
+
+	"github.com/a-h/templ"
+)
+
+func main() {
+	mux := http.NewServeMux()
+
+	// Serve static files from the "public" directory
+	mux.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("public"))))
+
+	// Handle the root route
+
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		// Render the Index template from the views package.
+		if err := views.Index().Render(r.Context(), w); err != nil {
+			log.Println("Error rendering view:", err)
+		}
+	})
+
+	log.Println("NimbleStack server started on :8080")
+	log.Fatal(http.ListenAndServe(":8080", mux))
+}
