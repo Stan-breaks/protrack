37f671843907b95a58632a43eb8c06c22b84cff5
Stan-breaks <stanleymwendwa03@gmail.com>
2025-02-17 18:37:41 +0300

    sqlc
    init

--- sqlc/schema.sql
+++ sqlc/schema.sql
@@ -4,5 +4,6 @@ CREATE TABLE users(
  email text NOT NULL,
  firstName text NOT NULL,
  lastName text NOT NULL,
- password text NOT NULL
+ password text NOT NULL,
+ created_at DATETIME DEFAULT CURRENT_TIMESTAMP
 );
