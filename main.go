83bf86288a87d9db5f6e205f3d8097329a34b516
Stan-breaks <stanleymwendwa03@gmail.com>
2025-02-18 14:52:51 +0300

    main.go
    intializing the db

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
 	"log"
 	"net/http"
 	"nimblestack/database"
@@ -11,17 +12,42 @@ import (
 	_ "github.com/mattn/go-sqlite3"
 )
 
+func initializeSchema(db *sql.DB) error {
+	schemaSQL, err := os.ReadFile("sqlc/schema.sql")
+	if err != nil {
+		return fmt.Errorf("failed to read schema file: %v", err)
+	}
+	tx, err := db.Begin()
+	if err != nil {
+		return fmt.Errorf("failed to begin transaction: %v", err)
+	}
+
+	_, err = tx.Exec(string(schemaSQL))
+	if err != nil {
+		return nil
+	}
+
+	if err := tx.Commit(); err != nil {
+		return fmt.Errorf("failed to commit schema: %v", err)
+	}
+
+	return nil
+}
+
 func main() {
 	db, err := sql.Open("sqlite3", "sqlite.db")
 	if err != nil {
 		log.Fatalf("Error opening database: %v", err)
 	}
-
 	defer db.Close()
-	queries := database.New(db)
 
-	jwtSercet := os.Getenv("API_TOKEN")
-	route := router.NewRouter(queries, []byte(jwtSercet))
+	if err := initializeSchema(db); err != nil {
+		log.Fatalf("Error initializing schema: %v", err)
+	}
+
+	queries := database.New(db)
+	jwtSecret := os.Getenv("API_TOKEN")
+	route := router.NewRouter(queries, []byte(jwtSecret))
 
 	log.Println("NimbleStack server started on :8080")
 	log.Fatal(http.ListenAndServe(":8080", route.Handler()))
