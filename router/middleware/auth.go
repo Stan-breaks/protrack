e5d1c19452e67078e1af3f1597ac1154d38cd3e8
Stan-breaks <stanleymwendwa03@gmail.com>
2025-03-05 16:56:23 +0300

    middleware
    auth

--- router/middleware/auth.go
+++ router/middleware/auth.go
@@ -3,25 +3,23 @@ package middleware
 import (
 	"fmt"
 	"net/http"
-	"os"
 
 	"github.com/golang-jwt/jwt"
 )
 
-func CheckAuth(next http.HandlerFunc) http.HandlerFunc {
+func CheckAuth(next http.HandlerFunc, jwtSecret []byte) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		cookie, err := r.Cookie("auth_token")
 		if err != nil {
 			http.Redirect(w, r, "/auth/login", http.StatusUnauthorized)
 			return
 		}
-		secretKey := []byte(os.Getenv("API_TOKEN"))
 		tokenString := cookie.Value
 		token, err := jwt.Parse(tokenString, func(token *jwt.Token) (any, error) {
 			if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
 				return nil, fmt.Errorf("unexpected signing method")
 			}
-			return secretKey, nil
+			return jwtSecret, nil
 		})
 		if !token.Valid || err != nil {
 			http.SetCookie(w, &http.Cookie{
