153a4295e03c9ae33917dc0f763da9a1832b6019
Stan-breaks <stanleymwendwa03@gmail.com>
2025-03-05 16:56:43 +0300

    sqlc
    queries

--- sqlc/query.sql
+++ sqlc/query.sql
@@ -4,3 +4,13 @@ userName,email,firstName,lastName,password
 ) VALUES (?,?,?,?,?) RETURNING *;
 -- name: GetUser :one
 SELECT * FROM users WHERE email =? LIMIT 1;
+-- name: GetAllUsers :many
+SELECT * FROM users;
+-- name: GetAllTables :many
+SELECT name FROM sqlite_schema;
+-- name: DeleteUser :exec
+DELETE FROM users WHERE id = ?;
+-- name: UpdateUser :exec
+UPDATE users 
+SET username = ?, password = ?
+WHERE id = ?;
