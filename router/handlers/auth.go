8ea680745ca7fb47285630921bfd1a50ee5caa77
Stan-breaks <stanleymwendwa03@gmail.com>
2025-03-06 13:41:50 +0300

    handler
    removing redudant code

--- router/handlers/auth.go
+++ router/handlers/auth.go
@@ -6,18 +6,11 @@ import (
 	"nimblestack/views"
 )
 
-func Login(w http.ResponseWriter, r *http.Request) {
+func Auth(w http.ResponseWriter, r *http.Request) {
 	if r.Method == http.MethodGet {
 		if err := views.Auth().Render(r.Context(), w); err != nil {
 			log.Println("Error rendering view:", err)
 		}
 	}
 
-}
-func Register(w http.ResponseWriter, r *http.Request) {
-	if r.Method == http.MethodGet {
-		if err := views.Auth().Render(r.Context(), w); err != nil {
-			log.Println("Error rendering view:", err)
-		}
-	}
 }
