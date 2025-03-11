5d8dc17b4f7185cc75634343664f72a868df7255
Stan-breaks <stanleymwendwa03@gmail.com>
2025-03-11 20:38:49 +0300

    api
    student login api

--- router/apis/auth.go
+++ router/apis/auth.go
@@ -44,46 +44,53 @@ func (h *AuthApi) Login(w http.ResponseWriter, r *http.Request) {
 		return
 
 	}
-	req := loginRequest{
-		Email:    r.FormValue("email"),
-		Password: r.FormValue("password"),
-	}
-	ctx, cancel := context.WithTimeout(r.Context(), 10*time.Second)
-	defer cancel()
-	user, err := h.Queries.GetUser(ctx, req.Email)
-	if err != nil {
-		if !errors.Is(err, sql.ErrNoRows) {
-			log.Printf("Database error: %v", err)
+	role := r.FormValue("role")
+	switch role {
+	case "student":
+		req := loginRequest{
+			Email:    r.FormValue("email"),
+			Password: r.FormValue("password"),
+		}
+		ctx, cancel := context.WithTimeout(r.Context(), 10*time.Second)
+		defer cancel()
+		user, err := h.Queries.GetStudent(ctx, req.Email)
+		if err != nil {
+			if !errors.Is(err, sql.ErrNoRows) {
+				log.Printf("Database error: %v", err)
+				http.Error(w, "Internal server error", http.StatusInternalServerError)
+				return
+			}
+		}
+		if err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(req.Password)); err != nil {
+			http.Error(w, "Invalid credentials", http.StatusUnauthorized)
+			return
+		}
+
+		token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+			"email": user.Email,
+			"exp":   time.Now().Add(h.tokenExp).Unix(),
+		})
+		tokenString, err := token.SignedString(h.jwtSecret)
+		if err != nil {
+			log.Printf("Token signing error: %v", err)
 			http.Error(w, "Internal server error", http.StatusInternalServerError)
 			return
 		}
-	}
-	if err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(req.Password)); err != nil {
-		http.Error(w, "Invalid credentials", http.StatusUnauthorized)
+		http.SetCookie(w, &http.Cookie{
+			Name:     "auth_token",
+			Value:    tokenString,
+			Path:     "/",
+			HttpOnly: true,
+			Expires:  time.Now().Add(h.tokenExp),
+			Secure:   true,
+			SameSite: http.SameSiteLaxMode,
+		})
+		w.WriteHeader(http.StatusOK)
+	default:
+		http.Error(w, "Invalid user role", http.StatusBadRequest)
+		log.Printf("invalid user role: %v\n", role)
 		return
 	}
-
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
-		"id":    user.ID,
-		"email": user.Email,
-		"exp":   time.Now().Add(h.tokenExp).Unix(),
-	})
-	tokenString, err := token.SignedString(h.jwtSecret)
-	if err != nil {
-		log.Printf("Token signing error: %v", err)
-		http.Error(w, "Internal server error", http.StatusInternalServerError)
-		return
-	}
-	http.SetCookie(w, &http.Cookie{
-		Name:     "auth_token",
-		Value:    tokenString,
-		Path:     "/",
-		HttpOnly: true,
-		Expires:  time.Now().Add(h.tokenExp),
-		Secure:   true,
-		SameSite: http.SameSiteLaxMode,
-	})
-	w.WriteHeader(http.StatusOK)
 }
 
 // register
@@ -97,61 +104,162 @@ func (h *AuthApi) Reqister(w http.ResponseWriter, r *http.Request) {
 		log.Printf("form error: %v\n", err)
 		return
 	}
-	req := database.CreateUserParams{
-		Username:  r.FormValue("Username"),
-		Email:     r.FormValue("email"),
-		Firstname: r.FormValue("firstname"),
-		Lastname:  r.FormValue("lastname"),
-		Password:  r.FormValue("password"),
+	role := r.FormValue("role")
+	switch role {
+	case "student":
+		req := database.CreateStudentParams{
+			Email:     r.FormValue("email"),
+			Firstname: r.FormValue("firstName"),
+			Lastname:  r.FormValue("Lastname"),
+			Password:  r.FormValue("password"),
+		}
+		hashedPassword, err := bcrypt.GenerateFromPassword([]byte(req.Password), bcrypt.DefaultCost)
+		if err != nil {
+			log.Printf("Password hashing error: %v", err)
+			http.Error(w, "Internal server error", http.StatusInternalServerError)
+			return
+		}
+		req.Password = string(hashedPassword)
+		ctx, cancel := context.WithTimeout(r.Context(), 10*time.Second)
+		defer cancel()
+		_, err = h.Queries.GetStudent(ctx, req.Email)
+		if err == nil {
+			http.Error(w, "Email already registered", http.StatusConflict)
+			return
+		} else if !errors.Is(err, sql.ErrNoRows) {
+			log.Printf("Database error: %v", err)
+			http.Error(w, "Internal server error", http.StatusInternalServerError)
+			return
+		}
+		if _, err := h.Queries.CreateStudent(ctx, req); err != nil {
+			log.Printf("User creation error: %v", err)
+			http.Error(w, "Failed to create user", http.StatusInternalServerError)
+			return
+		}
+		token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+			"email": req.Email,
+			"exp":   time.Now().Add(h.tokenExp).Unix(),
+		})
+
+		tokenString, err := token.SignedString(h.jwtSecret)
+		if err != nil {
+			log.Printf("Token signing error: %v", err)
+			http.Error(w, "Internal server error", http.StatusInternalServerError)
+			return
+		}
+		http.SetCookie(w, &http.Cookie{
+			Name:     "auth_token",
+			Value:    tokenString,
+			Path:     "/",
+			HttpOnly: true,
+			Secure:   true,
+			SameSite: http.SameSiteLaxMode,
+		})
+		w.WriteHeader(http.StatusOK)
+	case "coordinator":
+		req := database.CreateCoordinatorParams{
+			Email:     r.FormValue("email"),
+			Firstname: r.FormValue("firstName"),
+			Lastname:  r.FormValue("Lastname"),
+			Password:  r.FormValue("password"),
+		}
+		hashedPassword, err := bcrypt.GenerateFromPassword([]byte(req.Password), bcrypt.DefaultCost)
+		if err != nil {
+			log.Printf("Password hashing error: %v", err)
+			http.Error(w, "Internal server error", http.StatusInternalServerError)
+			return
+		}
+		req.Password = string(hashedPassword)
+		ctx, cancel := context.WithTimeout(r.Context(), 10*time.Second)
+		defer cancel()
+		_, err = h.Queries.GetStudent(ctx, req.Email)
+		if err == nil {
+			http.Error(w, "Email already registered", http.StatusConflict)
+			return
+		} else if !errors.Is(err, sql.ErrNoRows) {
+			log.Printf("Database error: %v", err)
+			http.Error(w, "Internal server error", http.StatusInternalServerError)
+			return
+		}
+		if _, err := h.Queries.CreateCoordinator(ctx, req); err != nil {
+			log.Printf("User creation error: %v", err)
+			http.Error(w, "Failed to create user", http.StatusInternalServerError)
+			return
+		}
+		token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+			"email": req.Email,
+			"exp":   time.Now().Add(h.tokenExp).Unix(),
+		})
+
+		tokenString, err := token.SignedString(h.jwtSecret)
+		if err != nil {
+			log.Printf("Token signing error: %v", err)
+			http.Error(w, "Internal server error", http.StatusInternalServerError)
+			return
+		}
+		http.SetCookie(w, &http.Cookie{
+			Name:     "auth_token",
+			Value:    tokenString,
+			Path:     "/",
+			HttpOnly: true,
+			Secure:   true,
+			SameSite: http.SameSiteLaxMode,
+		})
+		w.WriteHeader(http.StatusOK)
+	case "supervisor":
+		req := database.CreateSupervisorParams{
+			Email:     r.FormValue("email"),
+			Firstname: r.FormValue("firstName"),
+			Lastname:  r.FormValue("Lastname"),
+			Password:  r.FormValue("password"),
+		}
+		hashedPassword, err := bcrypt.GenerateFromPassword([]byte(req.Password), bcrypt.DefaultCost)
+		if err != nil {
+			log.Printf("Password hashing error: %v", err)
+			http.Error(w, "Internal server error", http.StatusInternalServerError)
+			return
+		}
+		req.Password = string(hashedPassword)
+		ctx, cancel := context.WithTimeout(r.Context(), 10*time.Second)
+		defer cancel()
+		_, err = h.Queries.GetStudent(ctx, req.Email)
+		if err == nil {
+			http.Error(w, "Email already registered", http.StatusConflict)
+			return
+		} else if !errors.Is(err, sql.ErrNoRows) {
+			log.Printf("Database error: %v", err)
+			http.Error(w, "Internal server error", http.StatusInternalServerError)
+			return
+		}
+		if _, err := h.Queries.CreateSupervisor(ctx, req); err != nil {
+			log.Printf("User creation error: %v", err)
+			http.Error(w, "Failed to create user", http.StatusInternalServerError)
+			return
+		}
+		token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+			"email": req.Email,
+			"exp":   time.Now().Add(h.tokenExp).Unix(),
+		})
+
+		tokenString, err := token.SignedString(h.jwtSecret)
+		if err != nil {
+			log.Printf("Token signing error: %v", err)
+			http.Error(w, "Internal server error", http.StatusInternalServerError)
+			return
+		}
+		http.SetCookie(w, &http.Cookie{
+			Name:     "auth_token",
+			Value:    tokenString,
+			Path:     "/",
+			HttpOnly: true,
+			Secure:   true,
+			SameSite: http.SameSiteLaxMode,
+		})
+		w.WriteHeader(http.StatusOK)
+	default:
+		http.Error(w, "Invalid user role", http.StatusBadRequest)
+		log.Printf("invalid user role: %v\n", role)
+
 	}
 
-	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(req.Password), bcrypt.DefaultCost)
-	if err != nil {
-		log.Printf("Password hashing error: %v", err)
-		http.Error(w, "Internal server error", http.StatusInternalServerError)
-		return
-	}
-
-	req.Password = string(hashedPassword)
-
-	ctx, cancel := context.WithTimeout(r.Context(), 10*time.Second)
-	defer cancel()
-
-	_, err = h.Queries.GetUser(ctx, req.Email)
-	if err == nil {
-		http.Error(w, "Email already registered", http.StatusConflict)
-		return
-	} else if !errors.Is(err, sql.ErrNoRows) {
-		log.Printf("Database error: %v", err)
-		http.Error(w, "Internal server error", http.StatusInternalServerError)
-		return
-	}
-
-	if _, err := h.Queries.CreateUser(ctx, req); err != nil {
-		log.Printf("User creation error: %v", err)
-		http.Error(w, "Failed to create user", http.StatusInternalServerError)
-		return
-	}
-
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
-		"email": req.Email,
-		"exp":   time.Now().Add(h.tokenExp).Unix(),
-	})
-
-	tokenString, err := token.SignedString(h.jwtSecret)
-	if err != nil {
-		log.Printf("Token signing error: %v", err)
-		http.Error(w, "Internal server error", http.StatusInternalServerError)
-		return
-	}
-
-	http.SetCookie(w, &http.Cookie{
-		Name:     "auth_token",
-		Value:    tokenString,
-		Path:     "/",
-		HttpOnly: true,
-		Secure:   true,
-		SameSite: http.SameSiteLaxMode,
-	})
-	w.WriteHeader(http.StatusOK)
 }
