895c09b8378f7c51babe4abd5afa49e51dcda260
Stan-breaks <stanleymwendwa03@gmail.com>
2025-03-15 09:14:16 +0300

    sqlc
    query

--- sqlc/query.sql
+++ sqlc/query.sql
@@ -14,7 +14,7 @@ email,firstName,lastName,password
 ) VALUES (?,?,?,?) RETURNING *;
 -- name: GetCoordinator :one
 SELECT * FROM coordinators WHERE email =? LIMIT 1;
---name: GetCoordinatorById :one
+-- name: GetCoordinatorById :one
 SELECT * FROM coordinators WHERE coordinatorId =? LIMIT 1;
 -- name: CreateSupervisor :one
 INSERT INTO supervisors(
@@ -22,7 +22,7 @@ email,firstName,lastName,password
 ) VALUES (?,?,?,?) RETURNING *;
 -- name: GetSupervisor :one
 SELECT * FROM supervisors WHERE email =? LIMIT 1;
---name: GetSupervisorById :one
+-- name: GetSupervisorById :one
 SELECT * FROM supervisors WHERE supervisorID =? LIMIT 1;
 -- name: GetAllSupervisors :many
 SELECT * FROM supervisors;
